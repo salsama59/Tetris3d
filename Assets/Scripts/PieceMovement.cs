c2831180f68c23f0d48a71824691c6ee43da26eb
salsama59 <saul.yeponde@gmail.com>
2018-01-31 14:38:44 +0100

    Initial source loading to launche the project.
    The player can now control an object on the screen

---
+++ Assets/Scripts/PieceMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceMovement : MonoBehaviour {
+
+    Rigidbody gameObJectRigidBody;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        gameObJectRigidBody =  this.gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float horiZontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        Vector3 newGameObjectVelocity = new Vector3();
+
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newGameObjectVelocity = new Vector3(horiZontalMove * speed, gameObJectRigidBody.velocity.y, gameObJectRigidBody.velocity.z);
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            newGameObjectVelocity = new Vector3(gameObJectRigidBody.velocity.x, gameObJectRigidBody.velocity.y, verticalMove * speed);
+        }
+        else
+        {
+            newGameObjectVelocity = new Vector3(gameObJectRigidBody.velocity.x, gameObJectRigidBody.velocity.y, -0.5f * speed);
+        }
+
+        gameObJectRigidBody.velocity = newGameObjectVelocity;
+
+    }
+}
