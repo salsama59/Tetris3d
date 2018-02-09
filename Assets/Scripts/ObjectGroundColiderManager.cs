c47c5fc18c010dfec35d07a767923b561fd932cb
salsama59 <saul.yeponde@gmail.com>
2018-02-09 14:38:24 +0100

    The whole physic system has been review, now there is a new type of object, the L pattern, the new colision système is still in developpement

--- Assets/Scripts/ObjectGroundColiderManager.cs
+++ Assets/Scripts/ObjectGroundColiderManager.cs
@@ -6,13 +6,33 @@ public class ObjectGroundColiderManager : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Piece"))
-        {
-            PieceMovement pieceMovementScript = other.GetComponent<PieceMovement>();
-            pieceMovementScript.IsMoving = false;
+        Debug.Log("now the piece : " + other.name + " is passing");
+        Debug.Log("tag is : " + other.tag);
 
-            Rigidbody objectColidingRigidBody = other.GetComponent<Rigidbody>();
-            objectColidingRigidBody.isKinematic = true;
+        if (other.CompareTag("Piece"))
+        {
+            Debug.Log("After comparing tags : " + other.tag + " is then " + "Piece?");
+            
+            PieceMovement pieceMovementScript = other.GetComponent<PieceMovement>();
+
+            if(pieceMovementScript.IsMoving)
+            {
+                Debug.Log("So pieceMovement variable Ismoving was : " + pieceMovementScript.IsMoving);
+                pieceMovementScript.IsMoving = false;
+
+                Debug.Log("So pieceMovement variable Ismoving is now : " + pieceMovementScript.IsMoving);
+
+                /*Rigidbody objectColidingRigidBody = other.GetComponent<Rigidbody>();
+                //objectColidingRigidBody.isKinematic = true;
+                Debug.Log("velocity was : " + objectColidingRigidBody.velocity);
+                objectColidingRigidBody.velocity = new Vector3(0, 0, 0);
+                Debug.Log("velocity is now : " + objectColidingRigidBody.velocity);*/
+            }
+
+        }
+        else if(other.CompareTag("Ground"))
+        {
+            return;
         }
 
         GameObject gameManagerObject = GameObject.FindGameObjectWithTag("GameManager");
