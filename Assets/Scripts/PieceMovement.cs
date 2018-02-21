56d0f7685abd0f47d815c0a62ca960f37465a255
salsama59 <saul.yeponde@gmail.com>
2018-02-21 14:10:23 +0100

    The movement smothness
    Now the objects are moving by maintaining the arrow keys and stop at the right position, some refactoring has been made in the pieceMovement script

--- Assets/Scripts/PieceMovement.cs
+++ Assets/Scripts/PieceMovement.cs
@@ -11,75 +11,123 @@ public class PieceMovement : MonoBehaviour {
     private Vector3 maxRange;
     private bool isMoving;
     public float speed;
+    private float elapsedTime;
+    public float targetElapsedtime;
+    public float timeToMoveToward;
 
     // Use this for initialization
-    void Start () {
-
+    void Start ()
+    {
         IsMoving = true;
         this.gameObjectTransform = this.gameObject.GetComponent<Transform>();
         this.gameObJectRigidBody = this.gameObject.GetComponent<Rigidbody>();
 
         this.CalculateFieldRanges();
-
     }
 
     // Update is called once per frame
-    void FixedUpdate () {
-        
+    void FixedUpdate ()
+    {
         if (IsMoving)
         {
+            elapsedTime += Time.deltaTime;
             float verticalMove = Input.GetAxis("Vertical");
 
             Vector3 newGameObjectVelocity = new Vector3();
 
             Vector3 newPosition = new Vector3();
 
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            if (Input.GetKey(KeyCode.RightArrow))
             {
                 newPosition = this.gameObjectTransform.position + Vector3.right;
-                this.gameObjectTransform.position = this.ClampObjectPosition(newPosition);
+                this.MoveObjectToNewPosition(newPosition);
             }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            else if (Input.GetKey(KeyCode.LeftArrow))
             {
                 newPosition = this.gameObjectTransform.position + Vector3.left;
-                this.gameObjectTransform.position = this.ClampObjectPosition(newPosition);
+                this.MoveObjectToNewPosition(newPosition);
             }
 
-            if(Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                this.gameObjectTransform.rotation *= Quaternion.Euler(0f, 90f, 0f);
-
-                if(this.gameObjectTransform.rotation.eulerAngles.y == 360f)
-                {
-                    this.gameObjectTransform.rotation = Quaternion.Euler(0f, 0f, 0f);
-                }
+                RotateObject(true);
             }
             else if(Input.GetKeyDown(KeyCode.LeftAlt))
             {
-                this.gameObjectTransform.rotation *= Quaternion.Euler(0f, -90f, 0f);
-
-                if (this.gameObjectTransform.rotation.eulerAngles.y == -360f)
-                {
-                    this.gameObjectTransform.rotation = Quaternion.Euler(0f, 0f, 0f);
-                }
+                this.RotateObject(false);
             }
 
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 newGameObjectVelocity = new Vector3(this.gameObJectRigidBody.velocity.x, this.gameObJectRigidBody.velocity.y, verticalMove * this.speed);
-                //newGameObjectVelocity.z = verticalMove * this.speed * Time.deltaTime;
-                //this.gameObjectTransform.Translate(newGameObjectVelocity, Space.Self);
             }
             else
             {
                 newGameObjectVelocity = new Vector3(this.gameObJectRigidBody.velocity.x, this.gameObJectRigidBody.velocity.y, -0.5f * this.speed);
-                //newGameObjectVelocity.z = -0.5f * this.speed * Time.deltaTime;
-                //this.gameObjectTransform.Translate(newGameObjectVelocity, Space.Self);
             }
 
             this.gameObJectRigidBody.velocity = newGameObjectVelocity;
         }
+    }
 
+    private void RotateObject(bool isClockwise)
+    {
+        Quaternion newrotation;
+        Quaternion rotationPreview;
+
+        if (isClockwise)
+        {
+            newrotation = Quaternion.Euler(0f, 90f, 0f);
+            rotationPreview = this.gameObjectTransform.rotation * newrotation;
+            if (rotationPreview.y == 360f)
+            {
+                newrotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+        }
+        else
+        {
+            newrotation = Quaternion.Euler(0f, -90f, 0f);
+            rotationPreview = this.gameObjectTransform.rotation * newrotation;
+            if (rotationPreview.y == -360f)
+            {
+                newrotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+        }
+        this.gameObjectTransform.rotation *= newrotation;
+    }
+
+    private void MoveObjectToNewPosition(Vector3 newPosition)
+    {
+        if (elapsedTime >= targetElapsedtime)
+        {
+            elapsedTime = 0;
+            this.gameObjectTransform.position = this.ClampObjectPosition(Vector3.Lerp(this.gameObjectTransform.position, newPosition, timeToMoveToward));
+        }
+    }
+
+    private Vector3 GetObjectSize(GameObject gameObject)
+    {
+        Vector3 objectSize = new Vector3();
+
+        Collider[] colliders = gameObject.GetComponents<Collider>();
+
+
+        foreach (var collider in colliders)
+        {
+            Vector3 colliderSize = collider.bounds.size;
+
+            objectSize.x += colliderSize.x;
+            objectSize.y += colliderSize.y;
+            objectSize.z += colliderSize.z;
+        }
+
+        return objectSize;
+
+    }
+
+    private Vector3 GetObjectHalfsize(GameObject gameObject)
+    {
+        return this.GetObjectSize(gameObject) * 0.5f;
     }
 
     void CalculateFieldRanges()
@@ -89,12 +137,15 @@ public class PieceMovement : MonoBehaviour {
         Vector3 objectScale = this.field.transform.localScale;
 
         this.maxRange = new Vector3(
-            objectHalfSize.x * objectScale.x - 0.5f,
-            objectHalfSize.y * objectScale.y - 0.5f,
-            objectHalfSize.z * objectScale.z - 0.5f
+            objectHalfSize.x * objectScale.x + this.field.transform.position.x - this.GetObjectHalfsize(this.gameObject).x,
+            objectHalfSize.y * objectScale.y + this.field.transform.position.y - this.GetObjectHalfsize(this.gameObject).y,
+            objectHalfSize.z * objectScale.z + this.field.transform.position.z - this.GetObjectHalfsize(this.gameObject).z
             );
 
-        this.minRange = new Vector3(this.maxRange.x * -1, this.maxRange.y * -1, this.maxRange.z * -1);
+        this.minRange = new Vector3(
+            this.field.transform.position.x - objectHalfSize.x * objectScale.x + this.GetObjectHalfsize(this.gameObject).x,
+            this.field.transform.position.y - objectHalfSize.y * objectScale.y + this.GetObjectHalfsize(this.gameObject).y,
+            this.field.transform.position.z - objectHalfSize.z * objectScale.z + this.GetObjectHalfsize(this.gameObject).z);
     }
 
     private Vector3 ClampObjectPosition(Vector3 position)
